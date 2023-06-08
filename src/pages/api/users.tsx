12e1c4f9f7188942b7b1d09eb4378f3708947510
AlphaH7 <Alistier.hea@gmail.com>
2023-06-08 16:09:08 +0530

    DEV: API layer updates

--- src/pages/api/users.tsx
+++ src/pages/api/users.tsx
@@ -1,15 +1,21 @@
 import axios from 'axios';
 
-export default async function handler(res: any) {
+type IRequest = {
+  body: any;
+  url: string;
+};
+
+export default async function handler(req: IRequest, res: any) {
   try {
     const users = await axios({
       url: process.env.MOCKY_EP,
       method: 'get',
+      data: req.body,
     });
-    // console.log(users.data);
+    console.log(users.data);
     return res.status(200).json(users.data);
   } catch (error: any) {
-    // console.log(error.response.data);
+    console.log(error.response.data);
     return res.status(error.response.status).json(error.response.data);
   }
 }
